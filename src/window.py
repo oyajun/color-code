a9fc969bcc3f41012cef28e2b5a34bd761c89b87
oyajun <oyamadajun@outlook.jp>
2024-03-29 23:49:10 +0900

    ボタンの追加

--- src/window.py
+++ src/window.py
@@ -17,14 +17,66 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw
-from gi.repository import Gtk
+from gi.repository import Adw, Gtk, Gio, GObject
 
 @Gtk.Template(resource_path='/com/oyajun/ColorCode/window.ui')
 class ColorCodeWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'ColorCodeWindow'
 
-    label = Gtk.Template.Child()
+    test_drop = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        list_store_expression = Gtk.PropertyExpression.new(
+                KeyValuePair,
+                None,
+                "value",
+        )
+
+
+        model = Gio.ListStore(item_type=KeyValuePair)
+        model.splice(
+            0, 0,
+            [
+                KeyValuePair(key=0, value="⬛ Black"),
+                KeyValuePair(key=1, value="🟫 Brown"),
+                KeyValuePair(key=2, value="🟥 Red"),
+                KeyValuePair(key=3, value="🟧 Orange"),
+                KeyValuePair(key=4, value="🟨 Yellow"),
+                KeyValuePair(key=5, value="🟩 Green"),
+                KeyValuePair(key=6, value="🟦 Blue"),
+                KeyValuePair(key=7, value="🟪 Violet"),
+                KeyValuePair(key=8, value="👽 Gray"),
+                KeyValuePair(key=9, value="⬜ White"),
+            ],
+        )
+
+        list_store_expression = Gtk.PropertyExpression.new(
+            KeyValuePair,
+            None,
+            "value",
+        )
+
+        self.test_drop.set_expression(list_store_expression)
+        self.test_drop.set_model(model)
+
+        self.test_drop.connect("notify::selected-item", self.on_advanced_selected_item)
+
+    def on_advanced_selected_item(self, _drop_down, _selected_item):
+        selected_item = _drop_down.get_selected_item()
+        if selected_item:
+            print(selected_item.key)
+
+class KeyValuePair(GObject.Object):
+    key = GObject.Property(
+        type=int,
+        flags=GObject.ParamFlags.READWRITE,
+    )
+    value = GObject.Property(
+        type=str,
+        nick="Value",
+        blurb="Value",
+        flags=GObject.ParamFlags.READWRITE,
+        default="",
+    )
