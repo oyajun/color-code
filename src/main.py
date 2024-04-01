1e486becb1925b719af5fe7cf876465b580b0334
oyajun <oyamadajun@outlook.jp>
2024-04-01 23:15:20 +0900

    jun→oyajun

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # main.py
 #
-# Copyright 2024 jun
+# Copyright 2024 oyajun
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
