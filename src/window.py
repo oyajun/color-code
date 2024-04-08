ff2a9854e8f03474281e9fe4168d8792f65652c1
Alex K <73014273+alexkdeveloper@users.noreply.github.com>
2024-04-08 11:52:06 +0800

    Add files via upload

--- src/window.py
+++ src/window.py
@@ -38,6 +38,13 @@ class ColorCodeWindow(Adw.ApplicationWindow):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        settings = Gio.Settings(schema_id="com.oyajun.ColorCode")
+
+        settings.bind("width", self, "default-width", Gio.SettingsBindFlags.DEFAULT)
+        settings.bind("height", self, "default-height", Gio.SettingsBindFlags.DEFAULT)
+        settings.bind("is-maximized", self, "maximized", Gio.SettingsBindFlags.DEFAULT)
+        settings.bind("is-fullscreen", self, "fullscreened", Gio.SettingsBindFlags.DEFAULT)
+
         list_store_expression = Gtk.PropertyExpression.new(
             KeyValuePair,
             None,
