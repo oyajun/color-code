c05174a9fc6458dc92b13285c3868239f14dc5d2
小山田 純 - Oyamada Jun <81346541+oyajun@users.noreply.github.com>
2024-04-15 00:16:53 +0900

    developer name FineFindus

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ class ColorCodeApplication(Adw.Application):
                                 application_icon='com.oyajun.ColorCode',
                                 developer_name='oyajun',
                                 version='0.1.1',
-                                developers=['oyajun','Alex K'],
+                                developers=['oyajun','Alex K','FineFindus'],
                                 copyright='© 2024 oyajun')
         about.present()
 
